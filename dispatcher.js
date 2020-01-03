ffc021f52d93d385953034f3e1a8fde2008cf4aa
Zhuang Xu <not-real@example.com>
2020-01-03 23:15:16 +1100

    Refactoring - split logic into a dispatcher module; enabled ESLint

---
+++ dispatcher.js
@@ -0,0 +1,52 @@
+const store = require('./store');
+
+const type = 'GAME_JOINED';
+
+const announcePeerReady = (playerInfo) => {
+  const { sockets } = store[playerInfo.sessionID];
+  const alivePeers = sockets.filter((s) => s.readyState === 1);
+  if (alivePeers.length > 1) {
+    alivePeers.forEach((s) => s.send(JSON.stringify({ type: 'PEER_READY' })));
+  }
+};
+
+const registerPlayer = (socket, payload) => {
+  const { sessionID } = payload;
+  const player = parseInt(payload.player, 10);
+  // eslint-disable-next-line no-restricted-globals
+  if (sessionID && !isNaN(player) && store[sessionID]) {
+    store[sessionID].sockets[player] = socket;
+    return { type, sessionID, player };
+  }
+
+  const idleSessions = Object.keys(store)
+    .filter((k) => store[k] && store[k].players.length < 2);
+  if (idleSessions.length > 0) {
+    store[idleSessions[0]].players.push(1);
+    store[idleSessions[0]].sockets.push(socket);
+    return { type, sessionID: idleSessions[0], player: 1 };
+  }
+
+  const newSessionID = Date.now();
+  store[newSessionID] = { players: [0], sockets: [socket] };
+  return { type, sessionID: newSessionID, player: 0 };
+};
+
+const dispatch = (socket, payload) => {
+  if (payload.type === 'JOIN_GAME') {
+    console.log('Store before', store);
+
+    const playerInfo = registerPlayer(socket, payload);
+    console.log('Allocated session', playerInfo);
+
+    console.log('Store after', store);
+
+    socket.send(JSON.stringify(playerInfo));
+
+    announcePeerReady(playerInfo);
+  } else {
+    socket.send(Date.now());
+  }
+};
+
+module.exports = { dispatch };
--- index.js
+++ index.js
@@ -1,53 +1,27 @@
-// Minimal amount of secure websocket server
 const fs = require('fs');
-const registerPlayer = require('./webSocketHelper');
-const store = require('./store');
 
-// read ssl certificate
 const privateKey = fs.readFileSync('ssl-cert/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('ssl-cert/fullchain.pem', 'utf8');
 
-const credentials = {key: privateKey, cert: certificate};
+const credentials = { key: privateKey, cert: certificate };
 const https = require('https');
 
-//pass in your credentials to create an https server
 const httpsServer = https.createServer(credentials);
 httpsServer.listen(8443);
 
 const WebSocketServer = require('ws').Server;
+const dispatcher = require('./dispatcher');
+
 const wss = new WebSocketServer({
   server: httpsServer,
 });
 
-function announcePeerReady(playerInfo) {
-  const { sockets } = store[playerInfo.sessionID];
-  const alivePeers = sockets.filter((s) => s.readyState === 1);
-  if (alivePeers.length > 1) {
-    alivePeers.forEach((s) => s.send(JSON.stringify({type: 'PEER_READY'})));
-  }
-}
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
     console.log('received: %s', message);
     try {
       const payload = JSON.parse(message);
-      // console.log(payload);
-      if (payload.type === 'JOIN_GAME') {
-
-        console.log('Store before', store);
-
-        const playerInfo = registerPlayer(ws, payload);
-        console.log('Allocated session', playerInfo);
-
-        console.log('Store after', store);
-
-        ws.send(JSON.stringify(playerInfo));
-
-        announcePeerReady(playerInfo);
-      } else {
-        ws.send(Date.now())
-      }
+      dispatcher.dispatch(ws, payload);
     } catch (e) {
       console.log(e);
     }
